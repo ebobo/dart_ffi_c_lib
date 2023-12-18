4e5e99dfb8d503d23c5b3b220850ec7ec257e79c
qi xu <qi.xu@carrier.com>
2023-12-18 13:41:01 +0100

    return -1 for am4_acesss_control

--- user_auth.c
+++ user_auth.c
@@ -34,25 +34,10 @@ bool am4_access_control_approve_new_current_user(char * user) {
 
 int am4_access_control_process_timeout(void) {
     // Implement logic
-    return 0;
+    return -1;
 }
 
 int am4_access_control_process_cancel(void) {
     // Implement logic
-    return 0;
-}
-
-
-// Function to determine user level based on username and password
-int get_user_level(const char* username, const char* password) {
-    // Example hardcoded users
-    if (strcmp(username, "admin") == 0 && strcmp(password, "admin123") == 0) {
-        return 3; // Admin level
-    } else if (strcmp(username, "operator") == 0 && strcmp(password, "op123") == 0) {
-        return 2; // Operational user
-    } else if (strcmp(username, "observer") == 0 && strcmp(password, "obs123") == 0) {
-        return 1; // Observation user
-    }
-
-    return 0; // User not found or wrong credentials
+    return -1;
 }
